867116eca18fe3d1c39f7a9e2b305d7e05a8563f
Nathanael Farley <nasfarley88@gmail.com>
2017-05-30 13:49:22 +0100

    Prep for storing messages.

--- python/bearded_duck/__init__.py
+++ python/bearded_duck/__init__.py
@@ -3,10 +3,9 @@ from telepot.helper import ChatContext
 from skybeard.beards import BeardChatHandler
 from skybeard.decorators import onerror
 from skybeard.utils import get_beard_config
+from skybeard.bearddbtable import BeardInstanceDBTable
 
 import random
-
-from collections import defaultdict
 
 CONFIG = get_beard_config()
 
@@ -21,7 +20,9 @@ class BeardedDuck(BeardChatHandler):
 
     _timeout = 10
 
-    # __init__ is implicit
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.explain_table = BeardInstanceDBTable(self, 'explain')
 
     @onerror()
     async def explain(self, msg):
@@ -40,9 +41,11 @@ async def _explain(beard_instance, msg):
     # Get a bot
     bot = await _create_bot(beard_instance)
 
-    await bot.sender.sendMessage("Explain yourbot! Type 'I'm done.' to finish.")
+    await bot.sender.sendMessage("Explain yourself! Type 'I'm done.' to finish.")
     while True:
         resp = await bot.listener.wait()
+        # TODO figure out how to store the responses in the self.explain_table
+        # for regurgitation
         if 'I\'m done.' == resp['text']:
             await bot.sender.sendMessage("Happy to help :)")
             return
